f5972a7d84060f8d6e1bfcd18caac8c367d444f1
Kartik Reddy <ikartik.reddy@gmail.com>
2025-01-08 19:41:30 +0530

    added a few basic lexer functions

--- lexer.py
+++ lexer.py
@@ -1,10 +1,31 @@
-import sys
 class lexer:
-    def __init__(self):
-        pass
-    
-    def nexttok(self):
+    def __init__(self,source):
+        self.source=source + '\n'
+        self.curChar=''
+        self.curPos=-1
+        self.nextChar()
+
+    def nextChar(self):
+        self.curPos+=1
+        if self.curPos<len(self.source):
+            self.curChar=self.source[self.curPos]
+        else:
+            self.curChar='\0'
+
+    def peek(self):
+        if self.curPos + 1 >= len(self.source):
+            return '\0'
+        return self.source[self.curPos+1]
+
+    def skipCow(self):
+        while self.curChar in ('\t',' '):
+            self.nextChar()
+
+        if self.curChar=='\\' and self.peek() == '\\':
+            while self.curChar!='\n':
+                self.nextChar()
+
+    def getToken(self):
         pass
 
-    def curtok(self):
-        pass
+class Token    
